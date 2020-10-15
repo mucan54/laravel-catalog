72f78f9ce2bc7ade0b93b7a83deb13187e89a3e9
Can Os <canoz@Can-MacBook-Air.home>
2020-10-15 20:22:22 +0300

    attribute listing

--- resources/views/attributes.blade.php
+++ resources/views/attributes.blade.php
@@ -1,8 +1,29 @@
 
-@foreach($projects->attributevalues as $cat)
-<li>
-    @if($cat->attribute)
-    {{$cat->attribute->name}}
-    @endif
-    {{$cat->name}}</li>
-@endforeach 
+<div id='specs'></div>
+<script src="/js/site/jquery.min.js"></script>
+@php
+$attr=[];
+foreach($projects->attributevalues as $cat){
+    $attr[$cat->attribute->id]=$cat->attribute->name;
+}
+ @endphp
+<script>
+
+
+var myelem=JSON.parse('@php echo json_encode($projects->attributevalues); @endphp');
+var array=[];
+var names=JSON.parse('@php echo json_encode($attr); @endphp');
+myelem.forEach(function(entry) {
+    if(!array[entry.attribute_id])
+    array[entry.attribute_id]=[];
+    array[entry.attribute_id].push(entry.name);
+});
+var html ='';
+
+for (var key in names) {
+    html+='<li>'+names[key]+': '+array[key].toString()+'</li>';
+}
+$('#specs').html(html);
+
+
+</script>
