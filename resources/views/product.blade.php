d0024c63ff891967c3c76f1ee9d2d3d6c2a8ad95
Can Os <canoz@Can-MacBook-Air.local>
2020-10-17 21:15:41 +0300

    swiper added

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -236,6 +236,27 @@ gtag('config', 'UA-120508683-2');
    $("#myimages").lightGallery(); 
 });
  
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoHeight: true, 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
  
     </script>
 </body>
